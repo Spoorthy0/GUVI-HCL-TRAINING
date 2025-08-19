52d0a0f5ebd1392a4cd59f415f462d9b4fb589dd
Spoorthy0 <spoorthyyedla@gmail.com>
2025-08-19 10:50:20 +0530

    Day 2

---
+++ Conditionals.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //int age=sc.nextInt()
+        // if(age>=18){
+        //     System.out.println("You are an adult");
+        // }
+        // else{
+        //     System.out.println("You are a minor");
+        // }
+
+
+        // int score=sc.nextInt();
+        // if(score>=90){
+        //     System.err.println("Grade A");
+        // }
+        // else if(score>=80){
+        //     System.out.println("Grade B");
+        // }
+        // else{
+        //     System.out.println("Grade C");
+        // }
+        
+        // int temp=-47;
+        // if(temp>=21){
+        //     System.out.println("Sunny day");
+        // }
+        // else if(temp<21){
+        //     System.out.println("Cloudy day");
+        // }
+        // else{
+        //     System.out.println("Rainy day");
+        // }
+
+        
+
+    }
+}
+
---
+++ OnetoTen.java
@@ -0,0 +1,32 @@
+public class OnetoTen {
+    public static void main(String[] args){
+        // for(int i=1;i<11;i++){
+        //     System.out.println(i);
+        // }
+        int i=1;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // while(i<11){
+        //     if(i%2!=0){
+        //         System.out.print(i+" ");
+        //     }
+        //     i++;
+        // }
+
+        // do { 
+        //     System.out.print(i+" ");
+        //     i++;
+        // } while (i<11);
+
+        for(i=1;i<=5;i++){
+            if(i==3)
+            continue;
+            System.out.println(i);
+
+        }
+    }
+    
+}
