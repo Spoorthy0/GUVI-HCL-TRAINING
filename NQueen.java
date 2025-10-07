9f4f3f72561170c1271a942df12d4b8addcb8787
Spoorthy0 <spoorthyyedla@gmail.com>
2025-10-07 19:09:26 +0530

    week 6

--- NQueen.java
+++ NQueen.java
@@ -23,7 +23,7 @@ public class NQueen {
             return;
         }
         for(int j=0;j<n;j++){
-            if(isSafe(board,row,col)){
+            if(isSafe(board,row,j)){
                 board[row][j]='Q';
                 nQueen(board, row+1);
                 board[row+1][j]='.';
@@ -31,5 +31,53 @@ public class NQueen {
             }
         }
 
+    }
+    public static boolean isSafe(char[][] board,int row,int col){
+        int n=board.length;
+        for(int j=0;j<n;j++){
+            if(board[row][j]=='Q'){
+                return false;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q') return false;
+        }
+        //upper right
+        int i=row;
+        int j=col;
+        while(i<=0 && j<n){
+            if(board[i][j]=='Q') return false;
+            i--;
+            j++;
+        }
+        //lower right
+        i=row;
+        j=col;
+        while(i<n && j<n){
+            if(board[i][j]=='Q') return false;
+            i++;
+            j++;
+        }
+        //Upper left
+
+        i=row;
+        j=col;
+        while(i>=0 && j>=0){
+            if(board[i][j]=='Q') return false;
+            i--;
+            j--;
+        }
+
+        //lower left
+
+        i=row;
+        j=col;
+        while(i<n && j>=0){
+            if(board[i][j]=='Q') return false;
+            i++;
+            j--;
+        }
+
+        return true;
     }
 }
